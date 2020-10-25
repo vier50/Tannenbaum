ae62e81c1651e6e8dccafbbb289f538f72699d05
smovo <drei20@freemail.hu>
2020-10-25 11:59:45 +0100

    more box options

--- src/main/kotlin/org/example/tannenbaum/BoxPattern.kt
+++ src/main/kotlin/org/example/tannenbaum/BoxPattern.kt
@@ -5,6 +5,7 @@ class BoxPattern : Pattern(3, "Box") {
     private var width = 5
     private var height = 3
     private var char = '*'
+    private var filled = false
 
     override fun init(): Pattern {
         print("Width of the box: ")
@@ -13,13 +14,19 @@ class BoxPattern : Pattern(3, "Box") {
         height = readLine()?.toIntOrNull() ?: height
         print("Char to print: ")
         char = readLine()?.firstOrNull() ?: char
+        print("Fill the box [true/false]: ")
+        filled = readLine()?.toBoolean() ?: filled
         return this
     }
 
     override fun draw() {
         println(char * width)
         for (i in 1..(height - 2)) {
-            println("*${' ' * (width - 2)}*")
+            if (filled) {
+                println(char * width)
+            } else {
+                println("*${' ' * (width - 2)}*")
+            }
         }
         println(char * width)
     }
