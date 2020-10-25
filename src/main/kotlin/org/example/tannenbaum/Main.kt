5a3310898fe245cc45727695a9bb0c17b1434c9d
smovo <drei20@freemail.hu>
2020-10-25 11:45:18 +0100

    updated pattern loop

--- src/main/kotlin/org/example/tannenbaum/Main.kt
+++ src/main/kotlin/org/example/tannenbaum/Main.kt
@@ -1,18 +1,25 @@
 package org.example.tannenbaum
 
+import kotlin.system.exitProcess
+
 fun main() {
     val patterns = mutableListOf<Pattern>()
     patterns.add(LeftShiftPyramidPattern())
+    patterns.add(RightShiftPyramidPattern())
 
     while (true) {
-        println("[0] Reload")
+        println("[0] Exit")
         for (pattern in patterns) {
             println("[${pattern.id}] ${pattern.name}")
         }
         println("[invalid] Reload")
 
         print("Choose a pattern to print: ")
-        val id = readLine()?.toIntOrNull() ?: 0
+        val id = readLine()?.toIntOrNull() ?: continue
+
+        if (id == 0) {
+            exitProcess(0)
+        }
 
         println("-".repeat(64))
         patterns.firstOrNull { it.id == id }?.init()?.draw()
